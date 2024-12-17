c938383ab10d08c719b97c9cdba0818a6808ee89
Boon Hian <csbhlim@gmail.com>
2024-12-17 12:16:33 +0800

    feat: split workspace to fe and be

---
+++ be/src/index.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+
+import express from 'express'
+import cors from 'cors'
+
+import GeneratorRouter from './routes/generate'
+
+dotenv.config()
+
+const port = process.env.SERVER_PORT || '8080'
+const app = express()
+
+app.use(express.json())
+
+// Allow CORS for localhost
+const corsOptions = {
+	origin: 'http://localhost:3000', // Replace with your frontend's URL if needed
+	methods: 'GET,PUT,PATCH,POST,DELETE',
+	credentials: true // Allow cookies or Authorization headers
+}
+
+app.use(cors(corsOptions))
+
+// Paths
+app.use('/', GeneratorRouter)
+
+app.listen(port, () => {
+	console.log(`server started at http://localhost:${port}`)
+})
