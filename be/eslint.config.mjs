848e08a6edc195e97101f0c44669508c0e054464
Boon Hian <csbhlim@gmail.com>
2025-01-10 21:17:35 +0800

    fix: eslint should ignore dist js files

--- be/eslint.config.mjs
+++ be/eslint.config.mjs
@@ -3,12 +3,15 @@ import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	{
+		ignores: ['dist/**']
+	},
 	{ files: ['**/*.{ts}'] },
 	{ languageOptions: { globals: globals.node } },
 	...tseslint.configs.recommended,
 	{
 		rules: {
-			// 'no-console': 'error'
+			'no-console': 'error'
 		}
 	}
 ]
